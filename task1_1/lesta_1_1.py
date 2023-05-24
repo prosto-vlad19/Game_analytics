e006533f5118b123e5eaabb3e23d7e803327a43f
vladislav-bychkov-innowise <vladislav.bychkov@innowise-group.com>
2023-05-24 22:31:39 +0300

    init

---
+++ task1_1/lesta_1_1.py
@@ -0,0 +1,396 @@
+import csv
+import os
+import sqlite3
+import warnings
+
+import matplotlib.cbook
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+warnings.filterwarnings(
+    "ignore", category=matplotlib.cbook.MatplotlibDeprecationWarning
+)
+
+# Подключение к базе данных
+conn = sqlite3.connect("../data/Dataset.db")
+cursor = conn.cursor()
+
+# Запрос 1: определение количества уникальных значений в столбце "team_build_type_id"
+unique_modes_query = "SELECT COUNT(DISTINCT team_build_type_id) FROM arenas"
+cursor.execute(unique_modes_query)
+unique_modes_count = cursor.fetchone()[0]
+print("Количество различных игровых режимов:", unique_modes_count)
+
+# Запрос 2: подсчет количества записей для каждого игрового режима
+count_modes_query = (
+    "SELECT team_build_type_id, COUNT(*) as count"
+    " FROM arenas"
+    " GROUP BY team_build_type_id"
+)
+cursor.execute(count_modes_query)
+count_maps_results = cursor.fetchall()
+
+# Вывод результатов
+print("Количество записей для каждого игрового режима:")
+for row in count_maps_results:
+    mode_id, count = row
+    print("Режим", mode_id, ": ", count, " записей")
+
+# Разделение результатов на два списка: режимы и количество записей
+modes = [row[0] for row in count_maps_results]
+counts = [row[1] for row in count_maps_results]
+
+# Создание графика с количеством записей для каждого игрового режима
+plt.bar(modes, counts)
+plt.xlabel("Игровой режим")
+plt.ylabel("Количество записей")
+plt.title("Популярность игровых режимов")
+plt.savefig("../task1_1/pictures/numb_of_rec_for_each_game_mode.png")
+plt.show()
+
+# Сохранение результатов в файл CSV
+filename = "../task1_1/data_proc/numb_of_rec_for_each_game_mode.csv"
+with open(filename, "w", newline="") as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(["Игровой режим", "Количество записей"])
+    for row in count_maps_results:
+        writer.writerow(row)
+
+
+print("Результаты сохранены в файл", filename)
+
+# Запрос 3: получение среднего количества заработанных опыта
+# и кредитов для каждого игрового режима
+
+bot_count_query = """
+SELECT team_build_type_id, AVG(exp) as avg_experience, AVG(credits) as avg_credits
+FROM arenas
+JOIN arena_members
+ON arenas.arena_id = arena_members.arena_id
+AND arenas.periphery_id = arena_members.periphery_id
+GROUP BY team_build_type_id
+"""
+
+cursor.execute(bot_count_query)
+avg_results = cursor.fetchall()
+
+# Join результатов двух запросов по team_build_type_id
+merged_results = []
+for avg_row in avg_results:
+    mode_id = avg_row[0]
+    avg_exp = avg_row[1]
+    avg_credits = avg_row[2]
+    count = next((row[1] for row in count_maps_results if row[0] == mode_id), 0)
+    merged_results.append((mode_id, avg_exp, avg_credits, count))
+
+# Вывод объединенных результатов
+print(
+    "Среднее количество заработанных опыта, кредитов и количество"
+    "записей для каждого игрового режима:"
+)
+for row in merged_results:
+    mode_id, avg_exp, avg_credits, count = row
+    print("Режим", mode_id)
+    print("Средний опыт:", avg_exp)
+    print("Средние кредиты:", avg_credits)
+    print("Количество записей:", count)
+    print()
+
+# Создание DataFrame из объединенных результатов
+df = pd.DataFrame(
+    merged_results,
+    columns=["Игровой режим", "Средний опыт", "Средние кредиты", "Количество записей"],
+)
+
+# Построение диаграммы
+
+fig, ax = plt.subplots()
+bar_width = 0.25
+
+x = np.arange(len(df["Игровой режим"]))
+ax.set_yscale("log")
+ax.bar(x, df["Средний опыт"], width=bar_width, label="Средний опыт")
+ax.bar(x + bar_width, df["Средние кредиты"], width=bar_width, label="Средние кредиты")
+ax.bar(
+    x + 2 * bar_width,
+    df["Количество записей"],
+    width=bar_width,
+    label="Количество записей",
+)
+
+ax.set_xlabel("Игровой режим")
+ax.set_ylabel("Значения")
+ax.set_title("Статистика игровых режимов")
+ax.set_xticks(x + bar_width)
+ax.set_xticklabels(df["Игровой режим"], rotation=45)
+ax.legend(loc="upper left", bbox_to_anchor=(1, 1))
+
+plt.tight_layout()
+plt.savefig("../task1_1/pictures/avg_exp_and_cred_for_each_game_mode.png")
+plt.show()
+
+# Сохранение таблицы с результатами в файл
+filename = "../task1_1/data_proc/avg_exp_and_cred_for_each_game_mode.csv"
+with open(filename, 'w', encoding='utf-8-sig') as file:
+    # Сохранение датафрейма в файл
+    df.to_csv(filename, index=False, encoding='utf-8-sig')
+print("Таблица с результатами сохранена в файл", filename)
+
+# Запрос 4: получение среднего количества ботов для каждого игрового режима
+bot_query = """
+SELECT team_build_type_id, AVG(CASE WHEN account_db_id < 0 THEN 1 ELSE 0 END)
+as avg_bots
+FROM arenas
+JOIN arena_members
+ON arenas.arena_id = arena_members.arena_id
+AND arenas.periphery_id = arena_members.periphery_id
+GROUP BY team_build_type_id
+"""
+
+# Выполнение запроса
+cursor.execute(bot_query)
+bot_results = cursor.fetchall()
+
+# Вывод результатов
+print("Среднее количество ботов для каждого игрового режима:")
+for row in bot_results:
+    mode_id, avg_bots = row
+    print("Режим", mode_id)
+    print("Среднее количество ботов:", avg_bots)
+    print()
+
+bot_results_new = []
+
+for bot_row, record_row in zip(bot_results, count_maps_results):
+    mode_id, avg_bots = bot_row
+    mode_id_record, count = record_row
+    if mode_id == mode_id_record:
+        bot_results_new.append((mode_id, avg_bots, count))
+
+# Построение диаграммы
+df = pd.DataFrame(
+    bot_results_new,
+    columns=["Игровой режим", "Среднее количество ботов", "Количество записей"],
+)
+fig, ax = plt.subplots()
+bar_width = 0.35
+
+x = df["Игровой режим"]
+ax.set_yscale("log")
+ax.bar(
+    x, df["Среднее количество ботов"], width=bar_width, label="Среднее количество ботов"
+)
+ax.bar(
+    x + bar_width, df["Количество записей"], width=bar_width, label="Количество записей"
+)
+
+ax.set_xlabel("Игровой режим")
+ax.set_ylabel("Значения")
+ax.set_title("Статистика ботов и записей в игровых режимах")
+ax.set_xticks(x + bar_width / 2)
+ax.set_xticklabels(df["Игровой режим"], rotation=45)
+ax.legend()
+
+plt.tight_layout()
+plt.savefig("../task1_1/pictures/avg_numb_of_bots_for_each_game_mode.png")
+plt.show()
+
+# Сохранение результатов в файл CSV
+filename = "../task1_1/data_proc/avg_numb_of_bots_for_each_game_mode.csv"
+with open(filename, "w", newline="") as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(["Игровой режим", "Среднее количество ботов"])
+    for row in bot_results_new:
+        writer.writerow(row)
+
+print("Результаты сохранены в файл", filename)
+
+# Запрос 5: получение средней продолжительности боя
+# и количество записей для каждого игрового режима
+query_day = """
+SELECT team_build_type_id, AVG(duration_sec) as avg_duration, COUNT(*) as count
+FROM arenas
+GROUP BY team_build_type_id
+"""
+
+# Выполнение запроса
+cursor.execute(query_day)
+results = cursor.fetchall()
+
+# Вывод результатов
+print("Средняя продолжительность боя и количество записей для каждого игрового режима:")
+for row in results:
+    mode_id, avg_duration, count = row
+    print("Режим", mode_id)
+    print("Средняя продолжительность боя (в секундах):", avg_duration)
+    print("Количество записей:", count)
+    print()
+
+# Построение диаграммы
+df = pd.DataFrame(
+    results,
+    columns=["Игровой режим", "Средняя продолжительность боя", "Количество записей"],
+)
+fig, ax = plt.subplots()
+bar_width = 0.35
+
+x = df["Игровой режим"]
+ax.set_yscale("log")
+ax.bar(
+    x,
+    df["Средняя продолжительность боя"],
+    width=bar_width,
+    label="Средняя продолжительность боя",
+)
+ax.bar(
+    x + bar_width, df["Количество записей"], width=bar_width, label="Количество записей"
+)
+
+ax.set_xlabel("Игровой режим")
+ax.set_ylabel("Значения")
+ax.set_title("Статистика продолжительности боя и записей в игровых режимах")
+ax.set_xticks(x + bar_width / 2)
+ax.set_xticklabels(df["Игровой режим"], rotation=45)
+ax.legend()
+
+plt.tight_layout()
+plt.savefig("../task1_1/pictures/avg_battle_durat_and_numb_of_rec_for_each_game_mode.png")
+plt.show()
+
+# Сохранение результатов в файл CSV
+filename = "../task1_1/data_proc/avg_battle_durat_and_numb_of_rec_for_each_game_mode.csv"
+with open(filename, "w", newline="") as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(
+        ["Игровой режим", "Средняя продолжительность боя", "Количество записей"]
+    )
+    for row in results:
+        writer.writerow(row)
+
+print("Результаты сохранены в файл", filename)
+
+# Запрос 6: получение дня недели боя и подсчета количества записей
+query_day = """
+SELECT CASE WHEN strftime('%w', start_dt) = '0' THEN '7'
+ELSE strftime('%w', start_dt) END as weekday, COUNT(*) as count
+FROM arenas
+GROUP BY weekday
+"""
+
+# Выполнение запроса
+cursor.execute(query_day)
+results = cursor.fetchall()
+
+# Вывод результатов
+print("День недели боя и количество записей:")
+for row in results:
+    weekday, count = row
+    print("День недели", weekday)
+    print("Количество записей:", count)
+    print()
+
+# Построение диаграммы
+df = pd.DataFrame(results, columns=["День недели", "Количество записей"])
+fig, ax = plt.subplots()
+x = df["День недели"]
+ax.bar(x, df["Количество записей"])
+
+ax.set_xlabel("День недели")
+ax.set_ylabel("Количество записей")
+ax.set_title("Статистика записей по дням недели")
+ax.set_xticks(x)
+ax.set_xticklabels(["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс"])
+
+plt.tight_layout()
+plt.savefig("../task1_1/pictures/numb_of_rec_by_day_of_the_week.png")
+plt.show()
+
+# Сохранение результатов в файл CSV
+filename = "../task1_1/data_proc/numb_of_rec_by_day_of_the_week.csv"
+with open(filename, "w", newline="") as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(["День недели", "Количество записей"])
+    for row in results:
+        writer.writerow(row)
+
+print("Результаты сохранены в файл", filename)
+
+
+# Запрос 7: подсчет количества записей для каждой карты
+count_maps_query = (
+    "SELECT map_type_id, COUNT(*) as count FROM arenas GROUP BY map_type_id"
+)
+cursor.execute(count_maps_query)
+count_maps_results = cursor.fetchall()
+
+# Вывод результатов
+print("Карта и количество записей:")
+for row in count_maps_results:
+    map_id, count = row
+    print("Карта ID:", map_id)
+    print("Количество записей:", count)
+    print()
+
+
+# Построение гистограммы
+df = pd.DataFrame(count_maps_results, columns=["Карта ID", "Количество записей"])
+plt.figure(figsize=(15, 7))
+x = df["Карта ID"]
+y = df["Количество записей"]
+
+plt.bar(x, y)
+plt.xlabel("Карта ID")
+plt.ylabel("Количество записей")
+plt.title("Статистика записей по картам")
+plt.xticks(x)
+
+plt.tight_layout()
+plt.savefig("../task1_1/pictures/numb_of_rec_for_each_map.png")
+plt.show()
+
+# Сохранение результатов в файл CSV
+filename = "../task1_1/data_proc/numb_of_rec_for_each_map.csv"
+with open(filename, "w", newline="") as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(["Карта ID", "Количество записей"])
+    for row in count_maps_results:
+        writer.writerow(row)
+
+print("Результаты сохранены в файл", filename)
+
+# Запрос 8: подсчет количества записей для каждого уровня боя
+count_levels_query = (
+    "SELECT battle_level_id, COUNT(*) as count FROM arenas GROUP BY battle_level_id"
+)
+cursor.execute(count_levels_query)
+count_lv_results = cursor.fetchall()
+
+# Построение диаграммы
+df = pd.DataFrame(count_lv_results, columns=["Уровень боя", "Количество записей"])
+plt.figure(figsize=(10, 6))
+x = df["Уровень боя"]
+y = df["Количество записей"]
+
+plt.bar(x, y)
+plt.xlabel("Уровень боя")
+plt.ylabel("Количество записей")
+plt.title("Статистика записей по уровням боя")
+
+plt.tight_layout()
+plt.savefig("../task1_1/pictures/numb_of_rec_for_each_battle_level.png")
+plt.show()
+
+# Сохранение результатов в файл CSV
+filename = "../task1_1/data_proc/numb_of_rec_for_each_battle_level.csv"
+with open(filename, "w", newline="") as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(["Уровень боя", "Количество записей"])
+    for row in count_lv_results:
+        writer.writerow(row)
+
+print("Результаты сохранены в файл", filename)
+
+# Закрытие соединения с базой данных
+cursor.close()
+conn.close()
---
+++ task1_2/lesta_1_2linear_regression_by_class.py
@@ -0,0 +1,72 @@
+import pandas as pd
+import warnings
+import matplotlib.cbook
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+import sqlite3
+from sklearn.preprocessing import RobustScaler
+
+warnings.filterwarnings(
+    "ignore", category=matplotlib.cbook.MatplotlibDeprecationWarning
+)
+
+#Попробуем сделать линейную регрессию для каждого класса кораблей в отдельности
+
+# Подключение к базе данных
+conn = sqlite3.connect('../data/Dataset.db')
+cursor = conn.cursor()
+#Извлекаем данные из таблицы arena_members
+
+query_different_class = "SELECT distinct(item_class) \
+FROM arena_members \
+INNER JOIN glossary_ships \
+ON arena_members.vehicle_type_id = glossary_ships.item_cd \
+"
+cursor.execute(query_different_class)
+classes = cursor.fetchall()
+print(classes)
+# Проходим по каждому классу
+for class_data in classes:
+    # Извлекаем данные из таблиц для текущего класса кораблей
+    query_class = f"SELECT ships_killed, planes_killed, damage, team_damage, \
+                    received_damage, regen_hp, is_alive, credits, exp \
+                    FROM arena_members \
+                    INNER JOIN glossary_ships \
+                    ON arena_members.vehicle_type_id = glossary_ships.item_cd \
+                    WHERE item_class = '{str(class_data[0])}'"
+    cursor.execute(query_class)
+    data = cursor.fetchall()
+
+    # Создаем DataFrame из полученных данных
+    columns = ['ships_killed', 'planes_killed', 'damage', 'team_damage', 'received_damage', 'regen_hp', 'is_alive',
+               'credits', 'exp']
+    df_class = pd.DataFrame(data, columns=columns)
+
+    scaler = RobustScaler()
+    # Выполняем нормализацию данных
+    df_class[columns] = scaler.fit_transform(df_class[columns])
+    df_copy = df_class.copy()
+
+    # Разделяем данные на независимые переменные (X) и зависимую переменную (y)
+    X = df_class.drop('ships_killed', axis=1)
+    y = df_class['ships_killed']
+
+    # Обучаем линейную регрессию
+    regression = LinearRegression()
+    regression.fit(X, y)
+
+    # Выводим важность каждой независимой переменной
+    importance = regression.coef_
+    print(f"Коэффициенты линейной регрессии для класса {str(class_data[0])}: {importance}")
+
+    # Визуализируем результаты
+    plt.figure(figsize=(16, 12))
+    plt.subplots_adjust(bottom=0.15, top=0.95)
+    plt.bar(columns[1:], importance)
+    plt.xticks(fontsize=14, rotation=30)
+    plt.yticks(fontsize=16)
+    plt.xlabel('Independent Variables', fontsize=18)
+    plt.ylabel('Coefficient', fontsize=18)
+    plt.title(f'Importance of Independent Variables (Class: {str(class_data[0])})', fontsize=18)
+    plt.savefig(f"../task1_2/pictures/linear_regression_ships_killed_class_{str(class_data[0])}.png")
+    plt.show()
