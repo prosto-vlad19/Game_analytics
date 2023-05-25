e69eb3674b7d8dea2a79f464b8089e1694f6bf13
vladislav-bychkov-innowise <vladislav.bychkov@innowise-group.com>
2023-05-25 13:01:02 +0300

    added RobustSkaled

--- task1_2/lesta_1_2_classification.py
+++ task1_2/lesta_1_2_classification.py
@@ -1,17 +1,18 @@
+import os
+import sqlite3
+
+import joblib
+import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 from sklearn.cluster import KMeans
-from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import silhouette_score
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report, silhouette_score
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report
-import matplotlib.pyplot as plt
-import sqlite3
-import os
-import joblib
+from sklearn.preprocessing import RobustScaler, StandardScaler
 
 # Подключение к базе данных
-conn = sqlite3.connect('../data/Dataset.db')
+conn = sqlite3.connect("../data/Dataset.db")
 cursor = conn.cursor()
 
 query = "SELECT vehicle_type_id,AVG(ships_killed),AVG(planes_killed),AVG(damage),\
@@ -23,16 +24,50 @@ cursor.execute(query)
 data = cursor.fetchall()
 
 # Создаем DataFrame из полученных данных
-columns = ['vehicle_type_id','AVG(ships_killed)', 'AVG(planes_killed)', 'AVG(damage)', 'AVG(team_damage)', 'AVG(received_damage)', 'AVG(regen_hp)', 'AVG(is_alive)', 'AVG(credits)', 'AVG(exp)']
+columns = [
+    "vehicle_type_id",
+    "AVG(ships_killed)",
+    "AVG(planes_killed)",
+    "AVG(damage)",
+    "AVG(team_damage)",
+    "AVG(received_damage)",
+    "AVG(regen_hp)",
+    "AVG(is_alive)",
+    "AVG(credits)",
+    "AVG(exp)",
+]
 df = pd.DataFrame(data, columns=columns)
 
 # Выбор необходимых столбцов
-selected_columns = ['AVG(ships_killed)', 'AVG(planes_killed)', 'AVG(damage)', 'AVG(team_damage)', 'AVG(received_damage)', 'AVG(regen_hp)', 'AVG(is_alive)', 'AVG(credits)', 'AVG(exp)']
+selected_columns = [
+    "AVG(ships_killed)",
+    "AVG(planes_killed)",
+    "AVG(damage)",
+    "AVG(team_damage)",
+    "AVG(received_damage)",
+    "AVG(regen_hp)",
+    "AVG(is_alive)",
+    "AVG(credits)",
+    "AVG(exp)",
+]
 data_selected = df[selected_columns]
 
-#TODO продумать нормализацию
+# необходимо для определения способа нормализации
+
+plt.figure(figsize=(16, 12))
+plt.subplots_adjust(bottom=0.15, top=0.95)
+plt.boxplot(data_selected)
+plt.xticks(
+    np.arange(1, len(selected_columns) + 1), selected_columns, fontsize=14, rotation=30
+)
+plt.yticks(fontsize=16)
+plt.yscale("log")  # Применение логарифмической шкалы по оси y
+plt.title("Box Plot of Data (Log Scale)", fontsize=18)
+plt.savefig("../task1_2/pictures/box_plot_classification.png")
+plt.show()
+
 # Масштабирование данных
-scaler = StandardScaler()
+scaler = RobustScaler()
 data_scaled = scaler.fit_transform(data_selected)
 
 # Определение оптимального количества кластеров
@@ -44,9 +79,9 @@ for k in range(1, 15):
 
 # Визуализация методом локтя
 plt.plot(range(1, 15), inertia)
-plt.xlabel('Number of Clusters')
-plt.ylabel('Inertia')
-plt.title('Elbow Method')
+plt.xlabel("Number of Clusters")
+plt.ylabel("Inertia")
+plt.title("Elbow Method")
 plt.show()
 
 # определение оптимального количества кластеров
@@ -59,46 +94,60 @@ for k in range(2, 15):
 
 # визуализация методом силуэта
 plt.plot(range(2, 15), silhouette_scores)
-plt.xlabel('Number of Clusters')
-plt.ylabel('Silhouette Score')
-plt.title('Silhouette Method')
+plt.xlabel("Number of Clusters")
+plt.ylabel("Silhouette Score")
+plt.title("Silhouette Method")
 plt.show()
 
 # Применение метода K-средних с оптимальным количеством кластеров
-k = 3
+k = 2
 kmeans = KMeans(n_clusters=k, random_state=0)
 kmeans.fit(data_scaled)
 
 # Добавление меток кластеров в исходные данные
-df['cluster_label'] = kmeans.labels_
+df["cluster_label"] = kmeans.labels_
 
 
 # Интерпретация результатов
-cluster_centers = scaler.inverse_transform(kmeans.cluster_centers_)  # Преобразование центроидов обратно в исходные значения
-cluster_labels = ['Cluster {}'.format(i) for i in range(k)]
+cluster_centers = scaler.inverse_transform(
+    kmeans.cluster_centers_
+)  # Преобразование центроидов обратно в исходные значения
+cluster_labels = ["Cluster {}".format(i) for i in range(k)]
 interpretation_data = pd.DataFrame(cluster_centers, columns=selected_columns)
-interpretation_data.insert(0, 'Cluster Label', cluster_labels)
+interpretation_data.insert(0, "Cluster Label", cluster_labels)
 
 # Вывод DataFrame с интерпретацией
 print(interpretation_data)
 
 # Добавление меток кластеров в исходные данные
-df['cluster_label'] = kmeans.labels_
+df["cluster_label"] = kmeans.labels_
 
 # Выбор переменных для классификации
-features = ['AVG(ships_killed)', 'AVG(planes_killed)', 'AVG(damage)', 'AVG(team_damage)', 'AVG(received_damage)', 'AVG(regen_hp)', 'AVG(is_alive)', 'AVG(credits)', 'AVG(exp)']
+features = [
+    "AVG(ships_killed)",
+    "AVG(planes_killed)",
+    "AVG(damage)",
+    "AVG(team_damage)",
+    "AVG(received_damage)",
+    "AVG(regen_hp)",
+    "AVG(is_alive)",
+    "AVG(credits)",
+    "AVG(exp)",
+]
 
-target = 'cluster_label'
+target = "cluster_label"
 
 # Разделение данных на обучающий и тестовый наборы
-X_train, X_test, y_train, y_test = train_test_split(df[features], df[target], test_size=0.2, random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(
+    df[features], df[target], test_size=0.2, random_state=0
+)
 
 # Создание и обучение классификационной модели
 classifier = LogisticRegression()
 classifier.fit(X_train, y_train)
 
 # Сохранение модели в файл
-model_filename = '../task1_2/data_proc/logistic_regression_model.joblib'
+model_filename = "../task1_2/data_proc/logistic_regression_model.joblib"
 joblib.dump(classifier, model_filename)
 
 # Прогнозирование классов на тестовом наборе данных
@@ -116,7 +165,17 @@ cursor.execute(query)
 data = cursor.fetchall()
 
 # Создаем DataFrame из полученных данных
-columns = ['AVG(ships_killed)', 'AVG(planes_killed)', 'AVG(damage)', 'AVG(team_damage)', 'AVG(received_damage)', 'AVG(regen_hp)', 'AVG(is_alive)', 'AVG(credits)', 'AVG(exp)']
+columns = [
+    "AVG(ships_killed)",
+    "AVG(planes_killed)",
+    "AVG(damage)",
+    "AVG(team_damage)",
+    "AVG(received_damage)",
+    "AVG(regen_hp)",
+    "AVG(is_alive)",
+    "AVG(credits)",
+    "AVG(exp)",
+]
 df = pd.DataFrame(data, columns=columns)
 
 # Масштабирование данных
