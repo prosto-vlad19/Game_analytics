2c474dc2d85fa9f02aa213aece787aa80a05b345
Vladislav <vlad.bychkov2001@mail.ru>
2023-11-23 16:56:28 +0300

    minor update histograms

--- task1_2/custom_metrics.py
+++ task1_2/custom_metrics.py
@@ -13,7 +13,7 @@ conn = sqlite3.connect("../data/Dataset.db")
 cursor = conn.cursor()
 
 # Выбор режима аналитики - для всех игроков (full) или не ботов (no_bots)
-mode = "no_bots"
+mode = "full"
 
 if mode == "full":
     postfix = ""
@@ -44,17 +44,16 @@ data = cursor.fetchall()
 columns = ["cat_name", "item_name", "avg_metric"]
 df = pd.DataFrame(data, columns=columns)
 
-
 # Построение гистограмм
 for cat_name, group_df in df.groupby("cat_name"):
     if cat_name == "PerfomanceTest":
         continue  # Skip building histogram for "PerformanceTest" category
 
-    plt.figure(figsize=(10, 6))  # Adjust the figure size
+    plt.figure(figsize=(12, 6))  # Adjust the figure size
 
     # Top 3 smallest values
     top3_smallest = group_df.nsmallest(3, "avg_metric")
-    plt.bar(
+    bars1 = plt.bar(
         top3_smallest["item_name"].astype(str),
         top3_smallest["avg_metric"],
         label="Top 3 Smallest",
@@ -64,7 +63,7 @@ for cat_name, group_df in df.groupby("cat_name"):
 
     # Top 3 largest values
     top3_largest = group_df.nlargest(3, "avg_metric")
-    plt.bar(
+    bars2 = plt.bar(
         top3_largest["item_name"].astype(str),
         top3_largest["avg_metric"],
         label="Top 3 Largest",
@@ -74,7 +73,6 @@ for cat_name, group_df in df.groupby("cat_name"):
 
     # Mean value
     mean_value = group_df["avg_metric"].mean()
-    plt.axhline(y=mean_value, color="red", linestyle="--", label="Mean Value")
 
     # Annotate each bar with its value
     for i, value in enumerate(top3_smallest["avg_metric"]):
@@ -100,24 +98,47 @@ for cat_name, group_df in df.groupby("cat_name"):
         )
 
     # Mean value annotation
+    plt.axhline(y=mean_value, color="red", linestyle="--", label="Mean Value")
     plt.text(
-        len(top3_smallest) / 2,
+        -0.5,  # Смещение в крайнее левое положение
         mean_value,
         f"Mean: {mean_value:.2f}",
-        ha="center",
-        va="bottom" if mean_value != 0 else "top",
-        color="green",
-        fontsize=8,
+        ha="left",
+        va="bottom",
+        color="black",  # Черный цвет шрифта
+        fontsize=10,  # Увеличенный размер шрифта
     )
 
-    plt.title(f"Histogram for {cat_name}")
+
+    # Annotation with metric formula
+    formula_annotation = (
+        "Formula: AVG((ships_killed + planes_killed) * is_alive)\n"
+        "This is the formula for calculating the killed metric."
+    )
+
+    # Calculate the height for the formula annotation
+    height = max(top3_largest["avg_metric"]) * 1.1
+    bbox = dict(boxstyle="round", alpha=0.1)
+    plt.annotate(
+        formula_annotation,
+        xy=(0, 1.02),
+        xycoords="axes fraction",
+        fontsize=8,
+        ha="left",
+        va="bottom",
+        bbox=bbox,
+    )
+
+    # Legend in the upper left corner
+    plt.legend(loc="upper left")
+
+    plt.title(f"Killed metric {cat_name}")
     plt.xlabel("item_name")
     plt.ylabel("avg_metric")
     plt.xticks(rotation=45, ha="right")  # Rotate x-axis labels
-    plt.legend()
     plt.tight_layout()  # Adjust layout for better spacing
+    plt.savefig(f"../task1_2/pictures/killed_metric_{cat_name}{postfix}.png")
     plt.show()
-
 
 # 2 damage metric
 # Query for average damage and received damage
@@ -148,7 +169,7 @@ df_avg_damage = pd.DataFrame(data_avg_damage, columns=columns_avg_damage)
 
 # Calculate new metric: damage / received damage
 df_avg_damage["damage_received_ratio"] = (
-    df_avg_damage["avg_damage"] / df_avg_damage["avg_received_damage"]
+        df_avg_damage["avg_damage"] / df_avg_damage["avg_received_damage"]
 )
 
 # Построение гистограмм для damage_received_ratio по каждой cat_name
@@ -156,7 +177,7 @@ for cat_name, group_df in df_avg_damage.groupby("cat_name"):
     if cat_name == "PerfomanceTest":
         continue  # Skip building histogram for "PerformanceTest" category
 
-    plt.figure(figsize=(10, 6))  # Adjust the figure size
+    plt.figure(figsize=(12, 6))  # Adjust the figure size
 
     # Check for ships with zero received damage
     zero_received_damage_ships = group_df[group_df["avg_received_damage"] == 0]
@@ -187,7 +208,7 @@ for cat_name, group_df in df_avg_damage.groupby("cat_name"):
 
     # Top 3 smallest values for damage_received_ratio
     top3_smallest_ratio = group_df.nsmallest(3, "damage_received_ratio")
-    plt.bar(
+    bars1 = plt.bar(
         top3_smallest_ratio["item_name"].astype(str),
         top3_smallest_ratio["damage_received_ratio"],
         label="Top 3 Smallest (damage_received_ratio)",
@@ -197,7 +218,7 @@ for cat_name, group_df in df_avg_damage.groupby("cat_name"):
 
     # Top 3 largest values for damage_received_ratio
     top3_largest_ratio = group_df.nlargest(3, "damage_received_ratio")
-    plt.bar(
+    bars2 = plt.bar(
         top3_largest_ratio["item_name"].astype(str),
         top3_largest_ratio["damage_received_ratio"],
         label="Top 3 Largest (damage_received_ratio)",
@@ -207,12 +228,6 @@ for cat_name, group_df in df_avg_damage.groupby("cat_name"):
 
     # Mean value for damage_received_ratio
     mean_ratio_value = group_df["damage_received_ratio"].mean()
-    plt.axhline(
-        y=mean_ratio_value,
-        color="red",
-        linestyle="--",
-        label="Mean Value (damage_received_ratio)",
-    )
 
     # Annotate each bar with its value for damage_received_ratio
     for i, value in enumerate(top3_smallest_ratio["damage_received_ratio"]):
@@ -237,48 +252,49 @@ for cat_name, group_df in df_avg_damage.groupby("cat_name"):
             fontsize=8,
         )
 
-    plt.title(f"Histogram for Damage Received Ratio - {cat_name}")
+    # Annotation with metric formula
+    formula_annotation = (
+        "Formula: AVG(damage) / AVG(received_damage)\n"
+        "This is the formula for calculating the damage_received_ratio."
+    )
+
+    # Calculate the height for the formula annotation
+    height = max(top3_largest_ratio["damage_received_ratio"]) * 1.1
+    bbox = dict(boxstyle="round", alpha=0.1)
+    plt.annotate(
+        formula_annotation,
+        xy=(0, 1.02),
+        xycoords="axes fraction",
+        fontsize=8,
+        ha="left",
+        va="bottom",
+        bbox=bbox,
+    )
+
+    # Mean value for damage_received_ratio
+    mean_ratio_value = group_df["damage_received_ratio"].mean()
+
+    # Annotate mean value at the extreme left position
+    plt.text(
+        -0.5,  # Смещение в крайнее левое положение
+        mean_ratio_value,
+        f"Mean: {mean_ratio_value:.2f}",
+        ha="left",
+        va="bottom",
+        color="black",  # Черный цвет шрифта
+        fontsize=10,  # Увеличенный размер шрифта
+    )
+
+    # Legend in the upper left corner
+    handles, labels = plt.gca().get_legend_handles_labels()
+    handles.append(plt.axhline(y=mean_ratio_value, color="red", linestyle="--"))
+    labels.append(f"Mean Value (damage_received_ratio): {mean_ratio_value:.2f}")
+    plt.legend(handles, labels, loc="upper left")
+
+    plt.title(f"Damage Received Ratio - {cat_name}")
     plt.xlabel("item_name")
     plt.ylabel("damage_received_ratio")
     plt.xticks(rotation=45, ha="right")  # Rotate x-axis labels
-    plt.legend()
     plt.tight_layout()  # Adjust layout for better spacing
-
-    # Separate histogram for ships with infinite values based on avg_damage
-    infinite_damage_ships = group_df[
-        group_df["avg_damage"].isin([np.inf, -np.inf])
-    ].copy()
-    infinite_damage_ships.reset_index(drop=True, inplace=True)
-
-    if not infinite_damage_ships.empty:
-        plt.figure(figsize=(10, 6))  # Adjust the figure size
-        plt.bar(
-            infinite_damage_ships["item_name"].astype(str),
-            infinite_damage_ships["avg_damage"],
-            label="Ships with Infinite avg_damage",
-            color="green",
-            alpha=0.7,
-        )
-
-        # Display information about the number of records for each ship
-        print(
-            f"Information about ships with infinite damage_received_ratio in {cat_name} category:"
-        )
-        print(
-            infinite_damage_ships[["item_name", "damage_received_ratio", "num_records"]]
-        )
-        infinite_damage_ships.to_csv(
-            f"infinite_damage_received_ratio_{cat_name}.csv", index=False
-        )  # Save to CSV
-
-        plt.title(
-            f"Histogram for Ships with Infinite damage_received_ratio - {cat_name}"
-        )
-        plt.xlabel("item_name")
-        plt.ylabel("avg_damage")
-        plt.xticks(rotation=45, ha="right")  # Rotate x-axis labels
-        plt.legend()
-        plt.tight_layout()  # Adjust layout for better spacing
-        plt.show()
-
+    plt.savefig(f"../task1_2/pictures/damage_metric_{cat_name}{postfix}.png")
     plt.show()
